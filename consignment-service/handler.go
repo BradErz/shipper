4184ff9ebcfd015ca5cf57ee078ac48e330b2ac5
Bradley <bwilsonhunt@gmail.com>
2018-01-18 17:10:44 +0000

    Implementing mongodb

---
+++ consignment-service/handler.go
@@ -0,0 +1,63 @@
+// consignment-service/handler.go
+package main
+
+import (
+	"log"
+
+	"golang.org/x/net/context"
+	"gopkg.in/mgo.v2"
+
+	pb "github.com/BradErz/shippy/consignment-service/proto/consignment"
+	vesselProto "github.com/BradErz/shippy/vessel-service/proto/vessel"
+)
+
+type service struct {
+	session *mgo.Session
+	vesselClient vesselProto.VesselServiceClient
+}
+
+func (s *service) GetRepo() Repository {
+	return &ConsignmentRepository{s.session.Clone()}
+}
+
+func (s *service) CreateConsignment(ctx context.Context, req *pb.Consignment, res *pb.Response) error {
+	repo := s.GetRepo()
+	defer repo.Close()
+
+	vesselResponse, err := s.vesselClient.FindAvailiable(context.Background(), &vesselProto.Specification{
+		MaxWeight: req.Weight,
+		Capacity: int32(len(req.Containers)),
+	})
+
+	if err != nil {
+		return err
+	}
+
+	log.Printf("found vessel: %s \n", vesselResponse.Vessel.Name)
+
+	req.VesselId = vesselResponse.Vessel.Id
+
+	err = repo.Create(req)
+
+	if err != nil {
+		return err
+	}
+
+	res.Created = true
+	res.Consignment = req
+	return nil
+}
+
+func (s *service) GetConsignments(ctx context.Context, req *pb.GetRequest, res *pb.Response) error {
+	repo := s.GetRepo()
+	defer repo.Close()
+
+	consignments, err := repo.GetAll()
+	if err != nil {
+		return err
+	}
+
+	res.Consignments = consignments
+	return nil
+
+}
--- consignment-service/main.go
+++ consignment-service/main.go
@@ -2,83 +2,33 @@
 package main
 
 import (
-	"context"
 	"log"
+	"os"
 
 	pb "github.com/BradErz/shippy/consignment-service/proto/consignment"
 	vesselProto "github.com/BradErz/shippy/vessel-service/proto/vessel"
 	"github.com/micro/go-micro"
 )
 
-type Repository interface {
-	Create(*pb.Consignment) (*pb.Consignment, error)
-	GetAll() []*pb.Consignment
-}
+const (
+	mongoDbHost = "localhost:27017"
+)
 
-// Repository - Dummy repository, this siulates the use of a datastore
-// of some kind. We'll replace this with a real implementation later on.
-type ConsignmentRepository struct {
-	consignments []*pb.Consignment
-}
-
-// Create a consignment
-func (repo *ConsignmentRepository) Create(consignment *pb.Consignment) (*pb.Consignment, error) {
-	updated := append(repo.consignments, consignment)
-	repo.consignments = updated
-	return consignment, nil
-}
-
-// GetAll
-func (repo *ConsignmentRepository) GetAll() []*pb.Consignment {
-	return repo.consignments
-}
-
-// Service should implement all of the methods to satisfy the service
-// we defined in our protobuf definition. You can check the interface
-// in the generated code itself for the exact method signatures etc
-// to give you a better idea.
-type service struct {
-	repo Repository
-	vesselClient vesselProto.VesselServiceClient
-}
-
-// CreateConsignment - we just created one method on our service.
-// which is a create method, which takes a context and a request as an
-// argument, these are handled by the gRPC server.
-func (s *service) CreateConsignment(ctx context.Context, req *pb.Consignment, res *pb.Response)  error  {
-	vesselResponse, err := s.vesselClient.FindAvailiable(context.Background(), &vesselProto.Specification{
-		MaxWeight: req.Weight,
-		Capacity: int32(len(req.Containers)),
-	})
-
-	log.Printf("found vessel: %s\n", vesselResponse.Vessel.Name)
-	if err != nil {
-		return err
-	}
-	req.VesselId = vesselResponse.Vessel.Id
-
-	// Save our consignment
-	consignment, err := s.repo.Create(req)
-	if err != nil {
-		return err
-	}
-
-	// Return matching the response we created in our proto definition
-	res.Created = true
-	res.Consignment = consignment
-	return nil
-
-}
-
-// GetConsignments - returns a list of all consignments
-func (s *service) GetConsignments(ctx context.Context, req *pb.GetRequest, res *pb.Response) error {
-	consignments := s.repo.GetAll()
-	res.Consignments = consignments
-	return nil
-}
 
 func main()  {
-	repo := &ConsignmentRepository{}
+	host := os.Getenv("DB_HOST")
+
+	if host == "" {
+		host = mongoDbHost
+	}
+
+	session, err :=  CreateSession(host)
+
+	defer session.Close()
+
+	if err != nil {
+		log.Panicf("could not connect to the db with host: %s - %v", host, err)
+	}
 
 	// Create a new service. Optionally include some options here.
 	srv := micro.NewService(
@@ -93,7 +43,7 @@ func main()  {
 	srv.Init()
 
 	// Register handler
-	pb.RegisterShippingServiceHandler(srv.Server(), &service{repo: repo, vesselClient: vesselClient})
+	pb.RegisterShippingServiceHandler(srv.Server(), &service{vesselClient: vesselClient})
 
 	// Run the server
 	if err := srv.Run(); err != nil {
